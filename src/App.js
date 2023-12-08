3cdaf25526dbf71a5ffa9341053a467d03952683
0xv1n4y <vinaysurisetty02@gmail.com>
2023-12-08 17:20:23 +0530

    newversion

--- src/App.js
+++ src/App.js
@@ -1,47 +1,214 @@
-import React,{useState} from 'react'
-import Gallery from './Gallery'
+import React, { useState } from "react";
+import Gallery from "./Gallery";
+import axios from "axios";
+import styled, { keyframes } from "styled-components";
+import { SearchOutlined } from "@mui/icons-material";
+import { mobile } from "./responsive";
 
-import axios from "axios"
-const apiKey ="636e1481b4f3c446d26b8eb6ebfe7127"
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
 
+const Container = styled.div`
+  background-color: #000000;
+  height: 100vh; /* 100% of the viewport height */
+  overflow-y: auto; /* Enable vertical scrolling if needed */
+  width: 100%; /* 100% of the parent's width */
+  position: sticky;
+  top: 0;
+  color: gray;
+`;
 
+const Navbar = styled.h1`
+  /* display: flex;
+  align-items: center;
+  justify-content: center; */
+  font-size: 40px;
+  font-weight: bold;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${mobile({ fontSize: "40px", padding: "10px" })}
+`;
 
+const Middle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 50px;
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  width: 50%;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  background: #2f3640;
+  border-radius: 50px;
+  position: relative;
+  ${mobile({ width: "80%" })}
+`;
+
+const Form = styled.form``;
+
+const SearchButton = styled.button`
+  color: white;
+  position: absolute;
+  right: 8px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: var(
+    --gradient-2,
+    linear-gradient(90deg, #2af598 0%, #009efd 100%)
+  );
+  border: 0;
+  display: inline-block;
+  margin: 10px 0px;
+  transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
+
+  /* hover effect */
+  &:hover {
+    color: #fff;
+    background-color: #1a1a1a;
+    box-shadow: rgba(0, 0, 0, 0.5) 0 10px 20px;
+    transform: translateY(-3px);
+  }
+
+  /* button pressing effect */
+  &:active {
+    box-shadow: none;
+    transform: translateY(0);
+  }
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  background: none;
+  width: 50;
+  outline: none;
+  color: white;
+  font-size: 20px;
+  padding: 24px 46px 24px 26px;
+
+  cursor: pointer;
+  ${mobile({ width: "50%" })}
+`;
+
+const Main = styled.div`
+  height: 100vh;
+`;
+const slideAnimation = keyframes`
+  0%, 100% {
+    bottom: -35px;
+  }
+
+  25%, 75% {
+    bottom: -2px;
+  }
+
+  20%, 80% {
+    bottom: 2px;
+  }
+`;
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(-15deg);
+  }
+
+  25%, 75% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(25deg);
+  }
+`;
+
+const LoaderContainer = styled.div`
+  width: 100px;
+  height: 100px;
+  position: relative;
+  background: #db9595;
+  border-radius: 4px;
+  overflow: hidden;
+  margin-left: 500px;
+  margin-top: 70px;
+
+  ${mobile({ margin: "80px 40px 0px 120px" })}
+`;
+
+const LoaderBefore = styled.div`
+  content: "";
+  position: absolute;
+
+  width: 40px;
+  height: 40px;
+  transform: rotate(45deg) translate(30%, 40%);
+  background: blue;
+  box-shadow: 32px -34px 0 5px #ff3d00;
+  animation: ${slideAnimation} 2s infinite ease-in-out alternate;
+`;
+
+const LoaderAfter = styled.div`
+  content: "";
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #ff3d00;
+  transform: rotate(0deg);
+  transform-origin: 35px 145px;
+  animation: ${rotateAnimation} 2s infinite ease-in-out;
+`;
 
 const App = () => {
-  const[search,setSearch]=useState("")
-  const[data,setData]=useState([])
-  const[sign,setSign]=useState("false")
-const changeHandler=(e=>{
-  setSearch(e.target.value)
-})
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
-const submitHandler=(e=>{
-  e.preventDefault();
-  
-     axios.get (`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
-    ).then(response=>
-    setData(response.data.photos.photo)
-  )
-  
-  
-})
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
+      )
+      .then((response) => setData(response.data.photos.photo));
+  };
   return (
-    <div className='app'>
-      <center>
-        <h1> Search For Wallpapers</h1>
-        <form onSubmit={submitHandler}>
-          <input size="30"type="text"  id="hello"name="search" value={search} onChange={changeHandler}/><br/><br/>
-          <input type="submit" name="search" id="hai"/><br/><br/>
-        </form>
-        <br/>
-        {
-          data.length>=1?<Gallery data={data}/>:<h1 style={{color:"Tomato"}}>Are You Looking For Somthing ?</h1>
-        }
-      </center>
-      
-    </div>
-  )
-}
+    <Container>
+      <Navbar>Picture Gallery</Navbar>
+      <Middle>
+        <SearchBox>
+          <Form onSubmit={submitHandler}>
+            <SearchInput
+              type="text"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <SearchButton type="submit" name="search">
+              <SearchOutlined />
+            </SearchButton>
+          </Form>
+        </SearchBox>
+      </Middle>
 
-export default App
+      <Main>
+        {data.length >= 1 ? (
+          <Gallery data={data} />
+        ) : (
+          <LoaderContainer>
+            <LoaderBefore />
+            <LoaderAfter />
+          </LoaderContainer>
+        )}
+      </Main>
+    </Container>
+  );
+};
+
+export default App;
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,22 +1,41 @@
-import React from 'react'
-import "./index.css"
+import React from "react";
+import "./index.css";
+import styled from "styled-components";
+import { mobile } from "./responsive";
 
-const Gallery = ({data}) => {
+const Container = styled.div`
+  width: 100%;
+  height: 550%;
+  background-color: black;
+  ${mobile({ margin: "40px 20px 10px -50px", position: "sticky" })}
+`;
+const ImageContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: 70px;
+  justify-content: space-around;
+`;
+const Image = styled.img`
+  height: 400px;
+  width: 500px;
+  padding: 10px;
+  ${mobile({ position: "sticky", height: "200px", width: "300px" })}
+`;
+
+const Gallery = ({ data }) => {
   return (
-    <div>
-        <div className='row'>
-            {data.map((image)=><div key={image.id}>
-                <div className='col-md-4'>
-                    
-                <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`} height="400" width="650"/>
-                </div>
+    <Container>
+      <ImageContainer>
+        {data.map((image) => (
+          <Image
+            key={image.id}
+            src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`}
+            alt="images"
+          />
+        ))}
+      </ImageContainer>
+    </Container>
+  );
+};
 
-            </div>)}
-
-        </div>
-      
-    </div>
-  )
-}
-
-export default Gallery
+export default Gallery;
